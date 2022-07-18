b6fa4534d573ba2cba005e92d3bccece41bfea2b
Josh Quaid <josh.quaid@gmail.com>
2022-07-18 09:02:14 -0500

    * Built tests * Final product executes with no errors

--- src/SpinWords.java
+++ src/SpinWords.java
@@ -1,5 +1,21 @@
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+public class SpinWords {
+
+    public String spinWords(String sentence) {
+        if (sentence.length() < 5) {
+            return sentence;
+        } else if (!sentence.contains(" ")) {
+            return String.valueOf(new StringBuilder(sentence).reverse());
+        } else {
+            // Split the sentence on whitespaces (one or more) then check to see if any
+            // elements have length >5 and if so, reverse those elements. Then, join the
+            // array of strings and return the spun sentence
+            String[] splitted = sentence.split("\\s+");
+            for (int word = 0; word < splitted.length; word++) {
+                if (splitted[word].length() > 4) {
+                    splitted[word] = String.valueOf(new StringBuilder(splitted[word]).reverse());
+                }
+            }
+            return String.join(" ", splitted);
+        }
     }
 }
--- src/SpinWordsTest.java
+++ src/SpinWordsTest.java
@@ -1,4 +1,15 @@
-import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 class SpinWordsTest {
-  
+
+    @Test
+    void spinWords() {
+        assertEquals("Hi", new SpinWords().spinWords("Hi"));
+        assertEquals("olleH", new SpinWords().spinWords("Hello"));
+        assertEquals("emocleW", new SpinWords().spinWords("Welcome"));
+        assertEquals("Hey wollef sroirraw", new SpinWords().spinWords("Hey fellow warriors"));
+        assertEquals("I went to the erots", new SpinWords().spinWords("I went to the store"));
+    }
 }
